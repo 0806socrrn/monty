81b014cec2b6619dd85529bfb292c84f3af42bda
yellyoshua <usuario1529yoshua@hotmail.com>
2022-05-18 21:17:46 -0500

    refactor and global variable

--- monty.h
+++ monty.h
@@ -42,12 +42,14 @@ typedef struct instruction_s
 } instruction_t;
 /** PROTOTYPES **/
 
+/** stack_actions.c **/
 void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
+
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 
 void sub(stack_t **stack, unsigned int line_number);
@@ -64,4 +66,5 @@ void stack(stack_t **stack, unsigned int line_number);
 void queue(stack_t **stack, unsigned int line_number);
 void add_node(stack_t **stack, unsigned int line_number);
 
+extern instruction_t global_stack;
 #endif
---
+++ stack_actions.c
@@ -0,0 +1,6 @@
+#include "monty.h"
+
+int main()
+{
+	global_stack.opcode = "asdasd";
+}
