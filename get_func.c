d44d862c98f10b17b0925250f7c2cdee406e090e
0806socrrn <socrrn@gmail.com>
2022-05-23 09:43:37 -0500

    updates

--- get_func.c
+++ get_func.c
@@ -1,6 +1,11 @@
 #include "monty.h"
-
-void (*get_method(char *method))(stack_t **stack, unsigned int line_number)
+/**
+ * get_method - identifies which method you want to apply to the data.
+ * @method: identifies the method.
+ * @line_number: number that enters.
+ */
+void (*get_method(char *method, unsigned int line_number))
+(stack_t **stack, unsigned int line_number)
 {
 	instruction_t methods[] = {
 			{"push", push},
@@ -9,15 +14,18 @@ void (*get_method(char *method))(stack_t **stack, unsigned int line_number)
 			{"pop", pop},
 			{"add", add},
 			{"swap", swap},
+
 			{NULL, NULL},
 	};
 
 	int i = 0;
+
 	while (methods[i].opcode != NULL)
 	{
 		if (strcmp(methods[i].opcode, method) == 0)
 			return (methods[i].f);
 		i++;
 	}
-	return (NULL);
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, method);
+	exit(EXIT_FAILURE);
 }
